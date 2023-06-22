10c885590c17cd6a5bd746e52234d7e2d7237250
Carlos Miguel <77444804+intcarlosmiguel@users.noreply.github.com>
2023-06-22 12:59:57 -0300

    Julia
    Julia

--- Projeto/main.jl
+++ Projeto/main.jl
@@ -21,28 +21,31 @@ Threads.nthreads() > 1 || error("Julia não está configurado para usar threads.
 
 
 
+# Vetores para armazenar os resultados
+ag_results = Vector{Any}(undef, 100)
+ls_results = Vector{Any}(undef, 100)
 x = 1
 
-tasks = Task[]
 Threads.@threads for seed in 1:100
     rede = criar_rede_erdos_renyi(N, p, 42 * x + seed)
-
-    t = Threads.@spawn  AG(rede, N, L, 42 * x + seed, 100, 400)
-    push!(tasks, t)
-
+    t = Threads.@spawn AG(rede, N, L, 42 * x + seed, 100, 400)
+    ag_results[seed] = fetch(t)
+    println(ag_results[seed])
 end
-for i in 1:100
-    println(arquivo,x,"\t", tasks[i])
-end
-tasks = Task[]
+
 x += 1
+
 Threads.@threads for seed in 1:100
     rede = criar_rede_erdos_renyi(N, p, 93 * x + seed)
-
-    t = Threads.@spawn  LS(rede, N, L, 93 * x + seed, 2, 20)[3]
-    push!(tasks, t)
-
+    t = Threads.@spawn LocalSearch(rede, N, L, 93 * x + seed, 2, 20)
+    ls_results[seed] = fetch(t)
 end
-for i in 1:100
-    println(arquivo,x,"\t", tasks[i])
+
+# Imprimir os resultados
+for seed in 1:100
+    println(arquivo, x, "\t", ag_results[seed])
+end
+
+for seed in 1:100
+    println(arquivo, x, "\t", ls_results[seed])
 end
